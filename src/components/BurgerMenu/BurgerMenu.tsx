d156654c5f816b634faec852352c8a74c19f0976
vitrokb <victor.brns2@gmail.com>
2024-07-17 16:23:41 -0300

    feat: navbar 100%styled and doing the active button

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,17 +1,33 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './BurgerMenu.css';
 import { StyledButton } from '../StyledButton';
 
 function BurgerMenu() {
+  const baseRoutes = {
+    inicio: 'false',
+    exposicao: 'false',
+    projeto: 'false',
+    bastidores: 'false',
+  };
+
   const [isOpen, setIsOpen] = useState(true);
+  const [locations, setLocations] = useState(baseRoutes);
+
+  const location = useLocation();
+
+  console.log(location.pathname);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    setLocations({...baseRoutes, [location.pathname]: location.pathname })
+  }, [])
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isOpen ? 'changeDirection' : ''}`}>
       <div className="burger" onClick={toggleMenu}>
         <div className={`line ${isOpen ? 'open' : ''}`}></div>
         <div className={`line ${isOpen ? 'open' : ''}`}></div>
@@ -20,24 +36,30 @@ function BurgerMenu() {
       <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
         <li>
           <Link to="/">
-            <StyledButton variant="outlined" active>
+            <StyledButton variant="outlined" active={locations.['/']}>
               INICIO
             </StyledButton>
           </Link>
         </li>
         <li>
           <Link to="/exposicao">
-            <StyledButton variant="outlined">EXPOSIÇÃO</StyledButton>
+            <StyledButton variant="outlined" active={locations.exposicao}>
+              EXPOSIÇÃO
+            </StyledButton>
           </Link>
         </li>
         <li>
           <Link to="/projeto">
-            <StyledButton variant="outlined">PROJETO</StyledButton>
+            <StyledButton variant="outlined" active={locations.projeto}>
+              PROJETO
+            </StyledButton>
           </Link>
         </li>
         <li>
           <Link to="/bastidores">
-            <StyledButton variant="outlined">BASTIDORES</StyledButton>
+            <StyledButton variant="outlined" active={locations.bastidores}>
+              BASTIDORES
+            </StyledButton>
           </Link>
         </li>
       </ul>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,10 @@
+import { useMediaQuery } from '@mui/material';
 import { BurgerMenu } from '../BurgerMenu';
 
 function Navbar() {
+  const matches = useMediaQuery('(max-width:670px)');
+
+  console.log(matches);
   return <BurgerMenu />;
 }
 
