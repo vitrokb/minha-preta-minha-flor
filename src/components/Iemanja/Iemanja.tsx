5a5801ee14c1ca080bb79904af4c372fb70e035d
vitrokb <victor.brns2@gmail.com>
2024-07-22 14:54:48 -0300

    feat: iemanja 100%

--- src/components/Iemanja/Iemanja.tsx
+++ src/components/Iemanja/Iemanja.tsx
@@ -1,25 +1,98 @@
 import { Typography } from '@mui/material';
 import Iemanja1 from '../../assets/iemanja-pictures/1.png';
+import Iemanja2 from '../../assets/iemanja-pictures/2.png';
+import Iemanja4 from '../../assets/iemanja-pictures/4.png';
+import Iemanja5 from '../../assets/iemanja-pictures/5.png';
+import Iemanja6 from '../../assets/iemanja-pictures/6.png';
+import Iemanja7 from '../../assets/iemanja-pictures/7.png';
+import Iemanja8 from '../../assets/iemanja-pictures/8.png';
+import Iemanja9 from '../../assets/iemanja-pictures/9.png';
+import Iemanja11 from '../../assets/iemanja-pictures/11.png';
+import Iemanja12 from '../../assets/iemanja-pictures/12.png';
+
 import './Iemanja.css';
+import Carousel from 'react-material-ui-carousel';
 
 function Iemanja() {
+  const pictures = [
+    { name: Iemanja2 },
+    { name: Iemanja4 },
+    { name: Iemanja6 },
+    { name: Iemanja7 },
+    { name: Iemanja8 },
+    { name: Iemanja9 },
+  ];
+
   return (
-    <div className="iemanja-first-section-wrapper">
-      <div className="iemanja-text">
-        <Typography variant="h3" sx={{ marginBottom: '1rem' }}>
-          Iemanjá
-        </Typography>
-        <Typography variant="body2" paragraph sx={{ textAlign: 'center' }}>
-          Como é lindo o canto de Iemanjá, faz até o pescador chorar, quem não escuta a mãe d'água
-          cantar, vai com ela pro fundo do mar.
-        </Typography>
-      </div>
-      <div className="iemanja-first-picture-wrapper">
-        <div className="iemanja-first-picture">
-          <img src={Iemanja1} />
+    <>
+      <div className="iemanja-first-section-wrapper">
+        <div className="iemanja-text">
+          <Typography variant="h3" sx={{ marginBottom: '1rem' }}>
+            Yemanjá
+          </Typography>
+          <Typography variant="body2" paragraph sx={{ textAlign: 'center' }}>
+            Como é lindo o canto de Yemanjá, faz até o pescador chorar, quem não escuta a mãe d'água
+            cantar, vai com ela pro fundo do mar.
+          </Typography>
+        </div>
+        <div className="iemanja-first-picture-wrapper">
+          <div className="iemanja-first-picture">
+            <img src={Iemanja1} />
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="iemanja-carrousel-wrapper">
+        <div className="iemanja-carrousel">
+          <Carousel
+            duration={1000}
+            interval={7000}
+            next={(next, active) => console.log(`we left ${active}, and are now at ${next}`)}
+            prev={(prev, active) => console.log(`we left ${active}, and are now at ${prev}`)}
+          >
+            {pictures.map((picture, key) => (
+              <img className="iemanja-carrousel-img" key={key} src={picture.name} />
+            ))}
+          </Carousel>
+        </div>
+      </div>
+
+      <div className="iemanja-second-section-wrapper">
+        <div className="iemanja-second-section-text">
+          <div className="">
+            <Typography
+              variant="h6"
+              sx={{
+                marginBottom: '1rem',
+                textAlign: 'center',
+                fontWeight: 700,
+                lineHeight: '1.375rem',
+              }}
+            >
+              THAIS BERNARDES <br></br> THAY BRAIDS
+            </Typography>
+          </div>
+          <Typography variant="body2" paragraph sx={{ textAlign: 'center' }}>
+            "YEMANJÁ PARA MIM É TUDO. ABAIXO DE DEUS FOI ELA QUEM ME TIROU DA DEPRESSÃO. FOI ELA QUE
+            ME LIVROU DE MUITAS COISAS. DEPOIS DE DESCOBRIR QUE ELA ERA MINHA MÃE. QUANDO DESCOBRI
+            QUE YEMANJÁ ERA MINHA MÃE. FOI A COISA MAIS LINDA DA MINHA VIDA. ELA MUDOU TOTALMENTE A
+            MINHA VIDA.“
+          </Typography>
+        </div>
+
+        <div className="iemanja-grid-wrapper">
+          <div className="iemanja-grid-item-1">
+            <img className="iemanja-grid-img" src={Iemanja11} />
+          </div>
+          <div className="iemanja-grid-item-2">
+            <img className="iemanja-grid-img" src={Iemanja12} />
+          </div>
+          <div className="iemanja-grid-item-3">
+            <img className="iemanja-grid-img" src={Iemanja5} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
