22e0581f2472073ac65510515df680038dc77d5f
vitrokb <victor.brns2@gmail.com>
2024-07-30 20:42:42 -0300

    feat: setup tests and Footer test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './setup.ts',
+    coverage: {
+      exclude: [
+        'src/**/index.tsx',
+        'src/**/index.ts',
+        'src/**/*.ts',
+        '.eslintrc.cjs',
+        'vite.config.ts',
+        'src/main.tsx',
+        'dist',
+      ],
+    },
+  },
+});
