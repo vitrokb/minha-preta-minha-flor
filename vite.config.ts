9d9314f067f5b410a7f41f936ef589c075cbfe09
vitrokb <victor.brns2@gmail.com>
2025-01-22 16:02:13 -0300

    fix: remove tests for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    css: true,
-    environment: 'happy-dom',
-    setupFiles: './setup.ts',
-    coverage: {
-      exclude: [
-        'src/**/index.tsx',
-        'src/**/index.ts',
-        'src/**/*.ts',
-        '.eslintrc.cjs',
-        'vite.config.ts',
-        'src/main.tsx',
-        'dist',
-      ],
-    },
-  },
 });
